5d07acadb610ee00e0c659ebd1a120e15aabc7ff
kristiyankisimov <kristiyankisimov@yahoo.com>
2014-06-15 17:52:44 +0300

    Delete old files

--- Exam1/src/com/hackbulgaria/corejava/IncorrectLevelException.java
+++
@@ -1,8 +0,0 @@
-package com.hackbulgaria.corejava;
-
-@SuppressWarnings("serial")
-public class IncorrectLevelException extends RuntimeException {
-    public IncorrectLevelException() {
-        super("Given non-positive level!");
-    }
-}
